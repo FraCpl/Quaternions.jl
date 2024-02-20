6f0fe3d24177aa52f2e599db267cadeb512237c6
FraCpl <fra.capolupo@gmail.com>
2024-02-20 09:48:43 +0100

    bugfix

--- src/rv.jl
+++ src/rv.jl
@@ -6,6 +6,6 @@ rv_fromDcm(R) = dcm_toRv(R)
 function rv_derivative(ϕ_AB, ωAB_B)
     # Bortz equation
     θ = norm(ϕ_AB)
-    ϕxω = ϕ × ωAB_B
-    return ωAB_B + 0.5*ϕxω + (1.0/θ^2 - 0.5/θ/tan(θ))* (ϕ × ϕxω)
+    ϕxω = ϕ_AB × ωAB_B
+    return ωAB_B + 0.5*ϕxω + (1.0/θ^2 - 0.5/θ/tan(θ))* (ϕ_AB × ϕxω)
 end
