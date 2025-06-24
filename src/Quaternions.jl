47e95de8848158163236386a221cbbe2dc1a4e14
FraCpl <fra.capolupo@gmail.com>
2025-06-24 16:45:07 +0200

    ups

--- src/Quaternions.jl
+++ src/Quaternions.jl
@@ -18,6 +18,7 @@ Compute the cross product matrix of a vector ```v```.
     return
 end
 @inline function crossMat!(R, v)
+    @inbounds for i in 1:3; R[i, i] = 0.0; end
     R[1, 2] = -v[3];    R[1, 3] = +v[2]
     R[2, 1] = +v[3];    R[2, 3] = -v[1]
     R[3, 1] = -v[2];    R[3, 2] = +v[1]
