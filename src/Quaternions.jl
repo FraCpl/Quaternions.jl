57bab495db7b38ddc27fbb435960d823d49a0a32
FraCpl <fra.capolupo@gmail.com>
2025-06-24 16:09:42 +0200

    add crossMat!

--- src/Quaternions.jl
+++ src/Quaternions.jl
@@ -3,7 +3,7 @@
 module Quaternions
 using LinearAlgebra
 
-export crossMat
+export crossMat, crossMat!
 """
     [v×] = crossMat(v)
 
@@ -11,6 +11,18 @@ Compute the cross product matrix of a vector ```v```.
 """
 @inline crossMat(v) = [0 -v[3] v[2]; v[3] 0 -v[1]; -v[2] v[1] 0]
 @inline crossMat(x, y, z) = [0 -z y; z 0 -x; -y x 0]
+@inline function crossMat!(R, x, y, z)
+    R[1, 2] = -z;    R[1, 3] = +y
+    R[2, 1] = +z;    R[2, 3] = -x
+    R[3, 1] = -y;    R[3, 2] = +x
+    return
+end
+@inline function crossMat!(R, v)
+    R[1, 2] = -v[3];    R[1, 3] = +v[2]
+    R[2, 1] = +v[3];    R[2, 3] = -v[1]
+    R[3, 1] = -v[2];    R[3, 2] = +v[1]
+    return
+end
 
 export crossMatInv
 @inline crossMatInv(M::Matrix) = [-M[2, 3]; M[1, 3]; -M[1, 2]]
